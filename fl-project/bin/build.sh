74da7a58d91f80e9c187c267bb03a2d3b206b4f7
alex <alex.gryaznof@gmail.com>
2018-04-28 18:42:46 +0300

    flproject bash scripts added

---
+++ fl-project/bin/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+CURRENT_DIR=`dirname "$0"`
+sh $CURRENT_DIR/shutdown.sh
+rm $CURRENT_DIR/fl-project.jar
+mvn -f $CURRENT_DIR/../pom.xml clean package -DskipTests
+cp $CURRENT_DIR/../target/fl-project.jar $CURRENT_DIR
+sh $CURRENT_DIR/startup.sh
