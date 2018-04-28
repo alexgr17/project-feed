65ce474a07af7c3efab8b47a927893bc8f018bbb
alex <alex.gryaznof@gmail.com>
2018-04-28 19:12:59 +0300

    swagger integration added

--- fl-project/src/main/java/ru/alexgryaznov/flproject/FlProjectApplication.java
+++ fl-project/src/main/java/ru/alexgryaznov/flproject/FlProjectApplication.java
@@ -8,19 +8,41 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 @EnableDiscoveryClient
 public class FlProjectApplication {
 
-	@LoadBalanced
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-		return restTemplateBuilder.build();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(FlProjectApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("ru.alexgryaznov.flproject")
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("ru.alexgryaznov.flproject"))
+				.paths(any())
+				.build()
+				.apiInfo(new ApiInfo("FL Project", null, "0.0.1-SNAPSHOT",
+						null, null, null, null, Collections.emptyList()));
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate internalRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
 	}
 }
