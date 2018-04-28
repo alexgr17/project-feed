ee26903efed58706dc2886e080b874bc9f92dd4c
alex <alex.gryaznof@gmail.com>
2018-04-28 20:13:28 +0300

    user timezone fixed

--- fl-project/bin/startup.sh
+++ fl-project/bin/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm nohup.out
-nohup java -Xmx128m -Xms128m -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -jar fl-project.jar &
+nohup java -Xmx128m -Xms128m -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Duser.timezone=Europe/Moscow -jar fl-project.jar &
