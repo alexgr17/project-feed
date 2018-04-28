9d6d4ffdbd4e8c35ac29929ad2ce6f448ce36313
alex <alex.gryaznof@gmail.com>
2018-04-28 19:45:51 +0300

    tables renamed - fixed

--- fl-project/src/main/java/ru/alexgryaznov/flproject/domain/KeyWord.java
+++ fl-project/src/main/java/ru/alexgryaznov/flproject/domain/KeyWord.java
@@ -1,10 +1,8 @@
 package ru.alexgryaznov.flproject.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "keyword_") //TODO rename table and remove table annotation
 public class KeyWord extends Word {
 
     public KeyWord() {
--- fl-project/src/main/java/ru/alexgryaznov/flproject/domain/StopWord.java
+++ fl-project/src/main/java/ru/alexgryaznov/flproject/domain/StopWord.java
@@ -1,10 +1,8 @@
 package ru.alexgryaznov.flproject.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "keyword") //TODO rename table and remove table annotation
 public class StopWord extends Word {
 
     public StopWord() {
